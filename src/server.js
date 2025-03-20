8252d5e540d06a7af734e75488b2917d5f4bf5ab
waleed0537 <133221815+waleed0537@users.noreply.github.com>
2025-03-20 23:02:31 +0500

    Changed

--- src/server.js
+++ src/server.js
@@ -276,7 +276,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
 // Start server
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
